bb169b6f99d03082c947314f73f60407949bdd05
FadyEmad01 <fadyemad933@gmail.com>
2026-02-15 22:13:23 +0200

    add pwa

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,23 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public', // Destination directory for the PWA files
+  // disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  // Your existing config here...
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
